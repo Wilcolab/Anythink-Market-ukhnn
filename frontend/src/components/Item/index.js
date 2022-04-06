df02fa2eb0bb3dcf70875907cce4a10398dc95b4
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-04-06 11:35:16 +0100

    adds placeholder image

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -1,13 +1,10 @@
-import ItemMeta from "./ItemMeta";
-import CommentContainer from "./CommentContainer";
-import React from "react";
-import agent from "../../agent";
-import { connect } from "react-redux";
-import marked from "marked";
-import {
-  ITEM_PAGE_LOADED,
-  ITEM_PAGE_UNLOADED,
-} from "../../constants/actionTypes";
+import ItemMeta from './ItemMeta';
+import CommentContainer from './CommentContainer';
+import React from 'react';
+import agent from '../../agent';
+import { connect } from 'react-redux';
+import marked from 'marked';
+import { ITEM_PAGE_LOADED, ITEM_PAGE_UNLOADED } from '../../constants/actionTypes';
 
 const mapStateToProps = (state) => ({
   ...state.item,
@@ -42,18 +39,21 @@ class Item extends React.Component {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
     const canModify =
-      this.props.currentUser &&
-      this.props.currentUser.username === this.props.item.seller.username;
+      this.props.currentUser && this.props.currentUser.username === this.props.item.seller.username;
     return (
       <div className="container page">
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src={
+                  this.props.item.image
+                    ? this.props.item.image
+                    : process.env.PUBLIC_URL + '/placeholder.png'
+                }
                 alt={this.props.item.title}
                 className="item-img"
-                style={{ height: "500px", width: "100%", borderRadius: "6px" }}
+                style={{ height: '500px', width: '100%', borderRadius: '6px' }}
               />
             </div>
 
--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import agent from "../agent";
-import { connect } from "react-redux";
-import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import agent from '../agent';
+import { connect } from 'react-redux';
+import { ITEM_FAVORITED, ITEM_UNFAVORITED } from '../constants/actionTypes';
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -30,15 +30,12 @@ const ItemPreview = (props) => {
   };
 
   return (
-    <div
-      className="card bg-dark border-light p-3"
-      style={{ borderRadius: "20px" }}
-    >
+    <div className="card bg-dark border-light p-3" style={{ borderRadius: '20px' }}>
       <img
         alt="item"
-        src={item.image}
+        src={item.image ? item.image : process.env.PUBLIC_URL + '/placeholder.png'}
         className="card-img-top item-img"
-        style={{ borderRadius: "20px" }}
+        style={{ borderRadius: '20px' }}
       />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
