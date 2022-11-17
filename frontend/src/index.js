3d77588c9984b658a89eba39c3b16f470191e142
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-11-17 16:24:48 +0000

    formatting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,30 @@
-import './custom.scss';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { store, history } from './store';
+import "./custom.scss";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import React from "react";
+import { store, history } from "./store";
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
+import {ThemeProvider} from 'styled-components'
 
-import { ThemeProvider } from 'styled-components';
+import App from "./components/App";
 
-import App from './components/App';
+const theme = {}
 
-const theme = {};
 
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </ConnectedRouter>
+
+    <ConnectedRouter history={history}>
+    
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
     </ThemeProvider>
   </Provider>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
