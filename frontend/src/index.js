ed3ed16e58eaa94ee0ca368606f360c7115298e0
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-11-17 17:56:09 +0000

    adds library and themeprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,21 @@ import { store, history } from "./store";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
+import { ThemeProvider } from "styled-components";
 
 import App from "./components/App";
 
+const theme = {};
+
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
+    <ThemeProvider theme={theme}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </ConnectedRouter>
+    </ThemeProvider>
   </Provider>,
 
   document.getElementById("root")
