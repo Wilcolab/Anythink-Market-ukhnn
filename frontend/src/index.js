eeb233001917248de1a567637175e449297fe634
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-11-17 16:18:29 +0000

    adds library and initial setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,28 @@
-import "./custom.scss";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import React from "react";
-import { store, history } from "./store";
+import './custom.scss';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { store, history } from './store';
 
-import { Route, Switch } from "react-router-dom";
-import { ConnectedRouter } from "react-router-redux";
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
-import App from "./components/App";
+import { ThemeProvider } from 'styled-components';
+
+import App from './components/App';
+
+const theme = {};
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
+    <ThemeProvider theme={theme}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </ConnectedRouter>
+    </ThemeProvider>
   </Provider>,
 
-  document.getElementById("root")
+  document.getElementById('root')
 );
