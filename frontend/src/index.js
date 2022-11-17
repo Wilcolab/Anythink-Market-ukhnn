c7445850e5c36c77da477b2ce39c265fdee12d1b
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-11-17 17:22:52 +0000

    in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,19 +8,16 @@ import { store, history } from "./store";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
-import { ThemeProvider } from "styled-components";
 
 import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider>
     <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={App} />
       </Switch>
     </ConnectedRouter>
-    </ThemeProvider>
   </Provider>,
 
   document.getElementById("root")
