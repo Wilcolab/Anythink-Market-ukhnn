13f89cf1f681abf183c78202b9bd130dee990ff3
Andre Ferreira <mrawesomeferreira@gmail.com>
2022-12-12 16:04:24 +0000

    test remove first pr

---
+++ frontend/src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ body {
+  background-image: url("../imgs/background.png");
+  background-position: top;
+  background-repeat: no-repeat;
+}
+`;
+
+export default GlobalStyle;
