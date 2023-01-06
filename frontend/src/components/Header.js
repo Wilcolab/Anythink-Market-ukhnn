a87aaa4c2987e843ce1221542df0098b1dd55e66
MrAFerreira <andre.f.v.ferreira@gmail.com>
2023-01-06 14:40:35 +0000

    refactors header to hook

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../imgs/topbar_logo.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../imgs/topbar_logo.png';
 
 const LoggedOutView = () => {
   return (
@@ -49,25 +49,20 @@ const LoggedInView = (props) => {
   );
 };
 
-class Header extends React.Component {
-  render() {
-    return (
-      <nav
-        className="navbar navbar-expand-md navbar-dark"
-        style={{ padding: "0.5rem 2rem" }}
-      >
-        <Link to="/" className="navbar-brand">
-          <img alt="logo" src={logo} />
-        </Link>
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark" style={{ padding: '0.5rem 2rem' }}>
+      <Link to="/" className="navbar-brand">
+        <img alt="logo" src={logo} />
+      </Link>
 
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
-        ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
-        )}
-      </nav>
-    );
-  }
-}
+      {this.props.currentUser ? (
+        <LoggedInView currentUser={this.props.currentUser} />
+      ) : (
+        <LoggedOutView currentUser={this.props.currentUser} />
+      )}
+    </nav>
+  );
+};
 
 export default Header;
