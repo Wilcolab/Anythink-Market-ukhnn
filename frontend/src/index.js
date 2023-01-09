2378adeff3cfe319ea091c1bbe5358b258d971ae
MrAFerreira <andre.f.v.ferreira@gmail.com>
2023-01-09 18:04:30 +0000

    lint fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,27 @@
-import './custom.scss';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { store, history } from './store';
+import "./custom.scss";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import React from "react";
+import { store, history } from "./store";
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
 
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+import {ThemeProvider} from 'styled-components'
+import theme from './styles/theme'
 
-import App from './components/App';
+import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <ThemeProvider theme={theme}>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
       </ThemeProvider>
     </ConnectedRouter>
   </Provider>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
