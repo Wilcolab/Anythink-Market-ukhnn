1838af1b06aa50f069ba2aa661191290043472bd
MrAFerreira <andre.f.v.ferreira@gmail.com>
2023-01-10 12:17:57 +0000

    refactors  banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,25 @@
-import React from "react";
-import logo from "../../imgs/logo.png";
+import React from 'react';
+import logo from '../../imgs/logo.png';
+import styled from 'styled-components';
+
+const StyledBanner = styled.div`
+  background-color: ${(props) => props.theme.dark};
+`;
 
 const Banner = () => {
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+    <StyledBanner>
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to </span>
+            <span id="get-part">get</span>
+            <span> the cool stuff.</span>
+          </div>
         </div>
       </div>
-    </div>
+    </StyledBanner>
   );
 };
 
