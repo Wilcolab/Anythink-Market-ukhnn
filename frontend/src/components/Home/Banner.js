5b8b38e6de009f12c20cca3314635e453c0f1abd
MrAFerreira <andre.f.v.ferreira@gmail.com>
2023-01-10 12:19:59 +0000

    lint fix

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from '../../imgs/logo.png';
-import styled from 'styled-components';
+import React from "react";
+import logo from "../../imgs/logo.png";
+import styled from "styled-components";
 
 const StyledBanner = styled.div`
-  background-color: ${(props) => props.theme.dark};
-`;
+background-color: ${props => props.theme.dark};
+`
 
 const Banner = () => {
   return (
     <StyledBanner>
-      <div className="banner text-white">
-        <div className="container p-4 text-center">
-          <img src={logo} alt="banner" />
-          <div>
-            <span>A place to </span>
-            <span id="get-part">get</span>
-            <span> the cool stuff.</span>
-          </div>
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span>A place to </span>
+          <span id="get-part">get</span>
+          <span> the cool stuff.</span>
         </div>
+      </div>
       </div>
     </StyledBanner>
   );
